d85e9237735f01382571c6b1430da39b70789d93
zek01svg <cia.velasco.2024@smu.edu.sg>
2025-10-02 12:42:19 +0800

    modified to use the stylesheet

--- myApp.js
+++ myApp.js
@@ -3,10 +3,11 @@ let app = express();
 
 let absolutePath = __dirname + '/views/index.html'
 
+app.use(express.static(__dirname, '/public'))
+
 app.get('/', (req, res) => {
     res.sendFile(absolutePath)
 })
-
 
 
 
