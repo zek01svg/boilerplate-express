845ed9494273da5f71afe9b3ddae2242b905252d
zek01svg <cia.velasco.2024@smu.edu.sg>
2025-10-02 12:35:40 +0800

    modified myapp.js

--- myApp.js
+++ myApp.js
@@ -1,6 +1,12 @@
 let express = require('express');
 let app = express();
 
+let absolutePath = __dirname + '/views/index.html'
+
+app.get('/', (req, res) => {
+    res.sendFile(absolutePath)
+})
+
 
 
 
