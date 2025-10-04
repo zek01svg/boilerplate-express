77d28d7bca990977c43ad83c4f0f1ec934bc2b35
zek01svg <cia.velasco.2024@smu.edu.sg>
2025-10-04 11:00:55 +0800

    finished problem 8

--- myApp.js
+++ myApp.js
@@ -1,46 +1,44 @@
 let express = require('express');
 let app = express();
+let path = require('path');
+const { nextTick } = require('process');
+require('dotenv').config()
 
-let absolutePath = __dirname + '/views/index.html'
+let absolutePath = path.join(__dirname, 'views', 'index.html');
 
-app.use(express.static(__dirname, '/public'))
+// Serve static files from the "public" folder
+app.use(express.static(path.join(__dirname, 'public')));
+
+// define the middleware function
+function consoleLogger (req,res,next){ 
+    // log the request method, path, and ip address to the console
+    console.log(`${req.method} - ${req.path} - ${req.ip}`)
+    next()
+}
+app.use(consoleLogger)
 
 app.get('/', (req, res) => {
-    res.sendFile(absolutePath)
+    res.sendFile(absolutePath);
+});
+
+// this route returns the time of the request in json
+app.get('/now', function logTime (req, res, next) {
+    req.time = new Date().toString()
+    next()
+}, function(req, res) {
+    res.json({"time": req.time})
 })
 
 
 
+app.get('/json', (req, res)=>{
+    if (process.env.MESSAGE_STYLE == 'uppercase') {
+        res.json({"message": "HELLO JSON"})
+    }
+    else {
+        res.json({"message": "Hello json"})
+    }
+    
+})
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
- module.exports = app;
+module.exports = app;
